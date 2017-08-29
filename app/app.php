97bd33114b591c14400bd44619b363ef2eed46e7
Kero76 <nic.gille@gmail.com>
2017-08-29 14:49:25 +0200

    :art: Update app.php and create Ticket entity.

--- app/app.php
+++ app/app.php
@@ -1,18 +1,73 @@
 <?php
-/**
- * Notepad.
- * Copyright (C) 2017 Nicolas GILLE
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+    /**
+     * Notepad.
+     * Copyright (C) 2017 Nicolas GILLE
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+
+    use Symfony\Component\Debug\ErrorHandler;
+    use Symfony\Component\Debug\ExceptionHandler;
+    use Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
+
+    declare(strict_types=1);
+
+    // Register global errors and exceptions handler.
+    ErrorHandler::register();
+    ExceptionHandler::register();
+
+    // Register Twig services providers.
+    $app->register(
+        new Silex\Provider\TwigServiceProvider(),
+        array(
+            'twig.path' => __DIR__ . '/../views/',
+        )
+    );
+
+    // Extends Twig with some services.
+    $app->extend(
+        'twig',
+        function(Twig_Environment $twig, $app) {
+            $twig->addExtension(new Twig_Extensions_Extension_Intl());
+            $twig->addExtension(new Twig_Extensions_Extension_Text());
+            return $twig;
+        }
+    );
+
+    // Register Asset service provider.
+    $app->register(
+        new Silex\Provider\AssetServiceProvider(),
+        array(
+            'assets.version' => 'v1',
+        )
+    );
+
+    // Register Doctrine ORM service provider.
+    $app->register(
+        new DoctrineOrmServiceProvider,
+        array(
+            'orm.proxies_dir' => 'src/App/Entity/Proxy',
+            'orm.auto_generate_proxies' => $app['debug'],
+            'orm.em.options' => array(
+                'mappings' => array(
+                    array(
+                        'type' => 'annotation',
+                        'namespace' => 'Notepad\\Entity\\',
+                        'path' => 'src/Entity',
+                        'use_simple_annotation_reader' => false,
+                    ),
+                ),
+            ),
+        )
+    );
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,116 @@
+<?php
+    /**
+     * Notepad.
+     * Copyright (C) 2017 Nicolas GILLE
+     *
+     * This program is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * This program is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+
+    namespace Notepad\Entity;
+
+    use Doctrine\ORM\Mapping as ORM;
+
+    /**
+     * Class Ticket.
+     *
+     * This class represent a ticket present on Notepad.
+     * In fact, each note is represent as a ticket with a title and the content of the ticket.
+     *
+     * @author Nicolas GILLE
+     * @package Notepad\Entity
+     * @since 0.1
+     * @version 1.0
+     */
+    class Ticket {
+
+        /**
+         * @var int
+         * @since 0.1
+         *
+         * @ORM\Id()
+         * @ORM\GeneratedValue(strategy="AUTO_INCREMENT")
+         * @ORM\Column(name="id", type="integer", nullable=false)
+         */
+        private $id;
+
+        /**
+         * @var string
+         * @since 0.1
+         *
+         * @ORM\Column(name="title", type="string")
+         */
+        private $title;
+
+        /**
+         * @var string
+         * @since 0.1
+         *
+         * @ORM\Column(name="content", type="text")
+         */
+        private $content;
+
+        /**
+         * @return int
+         * @since 0.1
+         * @version 1.0
+         */
+        public function getId(): int {
+            return $this->id;
+        }
+
+        /**
+         * @param int $id
+         * @since 0.1
+         * @version 1.0
+         */
+        public function setId(int $id) {
+            $this->id = $id;
+        }
+
+        /**
+         * @return string
+         * @since 0.1
+         * @version 1.0
+         */
+        public function getTitle(): string {
+            return $this->title;
+        }
+
+        /**
+         * @param string $title
+         * @since 0.1
+         * @version 1.0
+         */
+        public function setTitle(string $title) {
+            $this->title = $title;
+        }
+
+        /**
+         * @return string
+         * @since 0.1
+         * @version 1.0
+         */
+        public function getContent(): string {
+            return $this->content;
+        }
+
+        /**
+         * @param string $content
+         * @since 0.1
+         * @version 1.0
+         */
+        public function setContent(string $content) {
+            $this->content = $content;
+        }
+    }
