7c304cecf80c5f1fe8b4fa8d98f1bbefa9052f4c
Kero76 <nic.gille@gmail.com>
2017-08-30 16:21:40 +0200

    :art: Add user table and prepare translation files.

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS np_users;
+
+CREATE TABLE np_users (
+  usr_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  usr_name VARCHAR(50) NOT NULL,
+  usr_mail VARCHAR(255) NOT NULL,
+  usr_password VARCHAR(88) NOT NULL,
+  usr_salt VARCHAR(23) NOT NULL,
+  usr_role VARCHAR(50) NOT NULL
+) ENGINE = innodb CHARACTER SET utf8 COLLATE utf8_unicode_ci;
